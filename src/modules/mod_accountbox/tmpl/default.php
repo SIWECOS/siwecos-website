74f8d55f1e3a4180ca69dd93a2dc3c0e75c5783d
David Jardin <d.jardin@djumla.de>
2018-09-21 16:02:57 +0200

    Added languagestrings for accountbox

--- src/modules/mod_accountbox/tmpl/default.php
+++ src/modules/mod_accountbox/tmpl/default.php
@@ -15,19 +15,19 @@ defined('_JEXEC') or die; ?>
 <div class="accountbox" id="accountbox">
     <ul>
         <li class="accountbox__link accountbox__link--loggedoff">
-            <a href="<?php echo JURI::base(); ?>app/#/login">Anmelden</a>
+            <a href="<?php echo JURI::base(); ?>app/#/login"><?php echo JText::_('MOD_ACCOUNTBOX_LOGIN'); ?></a>
         </li>
         <li class="accountbox__link accountbox__link--loggedoff">
-            <a href="<?php echo JURI::base(); ?>app/#/register">Registrieren</a>
+            <a href="<?php echo JURI::base(); ?>app/#/register"><?php echo JText::_('MOD_ACCOUNTBOX_REGISTER'); ?></a>
         </li>
         <li class="accountbox__link accountbox__link--loggedin">
-            <a href="<?php echo JURI::base(); ?>app/#/account">Meine Daten</a>
+            <a href="<?php echo JURI::base(); ?>app/#/account"><?php echo JText::_('MOD_ACCOUNTBOX_ACCOUNT'); ?></a>
         </li>
         <li class="accountbox__link accountbox__link--loggedin">
-            <a href="<?php echo JURI::base(); ?>app/#/domains">Domainübersicht</a>
+            <a href="<?php echo JURI::base(); ?>app/#/domains"><?php echo JText::_('MOD_ACCOUNTBOX_OVERVIEW'); ?></a>
         </li>
         <li class="accountbox__link accountbox__link--loggedin">
-            <a id="accountbox__logoff" href="<?php echo JURI::base(); ?>app">Abmelden</a>
+            <a id="accountbox__logoff" href="<?php echo JURI::base(); ?>app"><?php echo JText::_('MOD_ACCOUNTBOX_LOGOUT'); ?></a>
         </li>
     </ul>
 </div>
