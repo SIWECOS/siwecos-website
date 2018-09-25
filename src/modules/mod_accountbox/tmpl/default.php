b3f9d71811301aac33db1aafbbab8ffa93d29a57
David Jardin <d.jardin@djumla.de>
2018-09-25 16:35:42 +0200

    Make accountbox link configurable

--- src/modules/mod_accountbox/tmpl/default.php
+++ src/modules/mod_accountbox/tmpl/default.php
@@ -15,19 +15,19 @@ defined('_JEXEC') or die; ?>
 <div class="accountbox <?php echo $moduleclassSfx; ?>" id="accountbox">
 	<ul>
 		<li class="accountbox__link accountbox__link--loggedoff">
-			<a href="<?php echo JURI::base(); ?>app/#/login"><?php echo JText::_('MOD_ACCOUNTBOX_LOGIN'); ?></a>
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/login"><?php echo JText::_('MOD_ACCOUNTBOX_LOGIN'); ?></a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedoff">
-			<a href="<?php echo JURI::base(); ?>app/#/register"><?php echo JText::_('MOD_ACCOUNTBOX_REGISTER'); ?></a>
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/register"><?php echo JText::_('MOD_ACCOUNTBOX_REGISTER'); ?></a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedin">
-			<a href="<?php echo JURI::base(); ?>app/#/account"><?php echo JText::_('MOD_ACCOUNTBOX_ACCOUNT'); ?></a>
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/account"><?php echo JText::_('MOD_ACCOUNTBOX_ACCOUNT'); ?></a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedin">
-			<a href="<?php echo JURI::base(); ?>app/#/domains"><?php echo JText::_('MOD_ACCOUNTBOX_OVERVIEW'); ?></a>
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/domains"><?php echo JText::_('MOD_ACCOUNTBOX_OVERVIEW'); ?></a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedin">
-			<a id="accountbox__logoff" href="<?php echo JURI::base(); ?>app"><?php echo JText::_('MOD_ACCOUNTBOX_LOGOUT'); ?></a>
+			<a id="accountbox__logoff" href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>"><?php echo JText::_('MOD_ACCOUNTBOX_LOGOUT'); ?></a>
 		</li>
 	</ul>
 </div>
