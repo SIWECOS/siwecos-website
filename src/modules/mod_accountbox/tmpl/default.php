5f0bb714c33f0f5d78b70ba73c6d1ee8bbef4b61
David Jardin <d.jardin@djumla.de>
2018-09-28 08:33:32 +0200

    cs fix

--- src/modules/mod_accountbox/tmpl/default.php
+++ src/modules/mod_accountbox/tmpl/default.php
@@ -15,16 +15,24 @@ defined('_JEXEC') or die; ?>
 <div class="accountbox <?php echo $moduleclassSfx; ?>" id="accountbox">
 	<ul>
 		<li class="accountbox__link accountbox__link--loggedoff">
-			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/login"><?php echo JText::_('MOD_ACCOUNTBOX_LOGIN'); ?></a>
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/login">
+				<?php echo JText::_('MOD_ACCOUNTBOX_LOGIN'); ?>
+			</a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedoff">
-			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/register"><?php echo JText::_('MOD_ACCOUNTBOX_REGISTER'); ?></a>
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/register">
+				<?php echo JText::_('MOD_ACCOUNTBOX_REGISTER'); ?>
+			</a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedin">
-			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/account"><?php echo JText::_('MOD_ACCOUNTBOX_ACCOUNT'); ?></a>
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/account">
+				<?php echo JText::_('MOD_ACCOUNTBOX_ACCOUNT'); ?>
+			</a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedin">
-			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/domains"><?php echo JText::_('MOD_ACCOUNTBOX_OVERVIEW'); ?></a>
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/domains">
+				<?php echo JText::_('MOD_ACCOUNTBOX_OVERVIEW'); ?>
+			</a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedin">
 			<a id="accountbox__logoff" href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>"><?php echo JText::_('MOD_ACCOUNTBOX_LOGOUT'); ?></a>
