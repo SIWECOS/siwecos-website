65ce46287ab64fe637e197fa6d979e25bdc6a5db
David Jardin <d.jardin@djumla.de>
2018-11-27 08:09:34 +0100

    Conditionally add matomo tracking code

--- src/templates/siwecos/index.php
+++ src/templates/siwecos/index.php
@@ -82,9 +82,9 @@ if (isset($this->_script['text/javascript']))
 	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=5">
 	<jdoc:include type="head" />
 	<style><?php echo $criticalCss; ?></style>
-    <!--[if IE ]>
-        <link href="<?php echo 'templates/' . $this->template . '/css/critical.css?v=' . md5(file_get_contents(dirname(__FILE__) . "/css/critical.css")); ?>" rel="stylesheet" type="text/css">
-    <![endif]-->
+	<!--[if IE ]>
+		<link href="<?php echo 'templates/' . $this->template . '/css/critical.css?v=' . md5(file_get_contents(dirname(__FILE__) . "/css/critical.css")); ?>" rel="stylesheet" type="text/css">
+	<![endif]-->
 </head>
 <body class="<?php echo $pageclass ? htmlspecialchars($pageclass) : 'default'; ?>">
 	<header>
@@ -151,5 +151,22 @@ if (isset($this->_script['text/javascript']))
 
 	<link href="<?php echo 'templates/' . $this->template . '/css/template.css?v=' . md5(file_get_contents(dirname(__FILE__) . "/css/template.css")); ?>" rel="stylesheet" type="text/css" />
 	<script async src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/template.js?v=<?php echo md5(file_get_contents(dirname(__FILE__) . "/js/template.js")); ?>"></script>
+	<?php if(JURI::base() == "https://siwecos.de/"): ?>
+	<!-- Matomo -->
+	<script>
+	 var _paq = _paq || [];
+	 /* tracker methods like “setCustomDimension” should be called before “trackPageView” */
+	 _paq.push(['trackPageView']);
+	 _paq.push(['enableLinkTracking']);
+	 (function() {
+	   var u="//webstats.eco.de/";
+	   _paq.push(['setTrackerUrl', u+'piwik.php']);
+	   _paq.push(['setSiteId', '152']);
+	   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	   g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+	 })();
+	</script>
+	<!-- End Matomo Code -->
+	<?php endif; ?>
 </body>
 </html>
