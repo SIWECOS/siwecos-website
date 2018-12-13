ab25935a5f67b591789186a302de08c70b8b88e6
David Jardin <d.jardin@djumla.de>
2018-12-13 18:59:45 +0100

    Fix accountbox links to avoid SIWECOS/webapp#17

--- src/modules/mod_accountbox/tmpl/default.php
+++ src/modules/mod_accountbox/tmpl/default.php
@@ -15,22 +15,22 @@ defined('_JEXEC') or die; ?>
 <div class="accountbox <?php echo $moduleclassSfx; ?>" id="accountbox">
 	<ul>
 		<li class="accountbox__link accountbox__link--loggedoff">
-			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/login">
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>#/login">
 				<?php echo JText::_('MOD_ACCOUNTBOX_LOGIN'); ?>
 			</a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedoff">
-			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/register">
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>#/register">
 				<?php echo JText::_('MOD_ACCOUNTBOX_REGISTER'); ?>
 			</a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedin">
-			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/account">
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>#/account">
 				<?php echo JText::_('MOD_ACCOUNTBOX_ACCOUNT'); ?>
 			</a>
 		</li>
 		<li class="accountbox__link accountbox__link--loggedin">
-			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>/#/domains">
+			<a href="<?php echo JRoute::_('index.php?Itemid=' . $params->get('menuitem')); ?>#/domains">
 				<?php echo JText::_('MOD_ACCOUNTBOX_OVERVIEW'); ?>
 			</a>
 		</li>
